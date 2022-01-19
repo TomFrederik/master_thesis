207a4070850a7993ee5fa6a91afde6e501f56d87
Tom Frederik <tlieberum@outlook.de>
2022-01-19 11:47:27 +0100

    create png

--- lava_gap/test.py
+++ lava_gap/test.py
@@ -11,4 +11,4 @@ env = MultiViewWrapper(env, percentages=[0.5, 0.5, 0.5])
 env = DropoutWrapper(env, dropout_p=0.1)
 obs = env.reset()
 
-display_helper(obs, save_path='./lava_gap/lava_all_views.pdf')
+display_helper(obs, save_path='./lava_gap/lava_all_views.png')
