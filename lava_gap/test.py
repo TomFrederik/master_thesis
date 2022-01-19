2321150059ac746b5f1c321aaef1369f156e9c3f
Tom Frederik <tlieberum@outlook.de>
2022-01-19 11:44:28 +0100

    Fixed test

--- lava_gap/test.py
+++ lava_gap/test.py
@@ -1,12 +1,14 @@
+import os
+
 import gym
 import numpy as np
 
-from lava_gap.utils import display_helper
-from lava_gap.wrappers import MultiViewWrapper, DropoutWrapper
+from utils import display_helper
+from wrappers import MultiViewWrapper, DropoutWrapper
 
 env = gym.make('MiniGrid-LavaCrossingS9N3-v0')
 env = MultiViewWrapper(env, percentages=[0.5, 0.5, 0.5])
 env = DropoutWrapper(env, dropout_p=0.1)
 obs = env.reset()
 
-display_helper(obs)
+display_helper(obs, save_path='./lava_gap/lava_all_views.pdf')
--- lava_gap/utils.py
+++ lava_gap/utils.py
@@ -1,12 +1,15 @@
-from typing import Dict
+from typing import Dict, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def display_helper(obs:Dict) -> None:
+def display_helper(
+    obs:Dict, 
+    save_path:Optional[str] = None
+) -> None:
     num_keys = len(obs.keys())
-    fig, axes = plt.subplots(1, num_keys, sharey=True)
+    fig, axes = plt.subplots(1, num_keys, sharey=True, )
     
     for i, key in enumerate(obs.keys()):
         if obs[key] is None:
@@ -14,10 +17,13 @@ def display_helper(obs:Dict) -> None:
         else:
             image = obs[key].transpose(1,0)
 
-        axes[i].imshow(image)
+        axes[i].imshow(image, cmap='inferno')
         axes[i].set_title(key)
         axes[i].set_axis_off()
 
-    plt.subplots_adjust(wspace=0, hspace=0)
-
+    plt.tight_layout()
+    plt.subplots_adjust(top=1, bottom=0, wspace=0, hspace=0)
+    # plt.margins(0,0)
+    if save_path is not None:
+        plt.savefig(save_path, pad_inches=0.1, bbox_inches='tight')
     plt.show()
