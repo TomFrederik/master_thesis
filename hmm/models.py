fix scaling of loss with nondropped

hmm/models.py
@@ -66,8 +66,7 @@ class DiscreteNet(nn.Module):
         obs_logits = self.emission(obs_frame, state_bit_vecs)
         obs_probs = F.softmax(obs_logits, dim=1) # (batch, num_states, num_views)
         posterior = prior
-        # for view in range(obs_probs.shape[-1]):
-        for view in range(2):
+        for view in range(obs_probs.shape[-1]):
             update = obs_probs[...,view] * (1 - dropped)[:, None, view]
             # print(f"update {view}: {update = }")
             nonzeros = torch.sum(update > 1e-6, dim=1)
@@ -249,18 +248,11 @@ class DiscreteNet(nn.Module):
         # obs_logits_sequence has shape (batch, seq_len, num_active_state, num_views)
         # dropped has shape (batch, seq_len, num_views)
         recon_loss = 0
-        # print(f"{posterior_belief_sequence.shape = }")
-        # print(f"{obs_logits_sequence.shape = }")
-        # print(f"{dropped.shape = }")
-        # print(f"{nonterms.shape = }")
         if not self.disable_recon_loss:
-            for view in range(dropped.shape[-1]):
-                # if view != 0:
-                #     continue
-                view_loss = ((-(1-dropped)[:,:,None,view] * posterior_belief_sequence * obs_logits_sequence[...,view]).sum(dim=2) * nonterms).sum(dim=-1).mean()
-                recon_loss = recon_loss + view_loss
-                
-            # recon_loss = ((-(1-dropped)[:,:,None,:] * posterior_belief_sequence[...,None] * obs_logits_sequence).sum(dim=[2,3]) * nonterms).sum(dim=-1).mean()
+            recon_loss = (-(1-dropped) * (posterior_belief_sequence[...,None] * obs_logits_sequence).sum(dim=2) * nonterms[...,None]).sum(dim=-1)
+            num_non_dropped = (1-dropped).sum(dim=-1)
+            recon_loss[num_non_dropped > 0] = recon_loss[num_non_dropped > 0] / num_non_dropped[num_non_dropped > 0]
+            recon_loss = recon_loss.sum(dim=-1).mean()
         return recon_loss
 
     def compute_vp_loss(self, value_prefix_pred, target_value_prefixes):