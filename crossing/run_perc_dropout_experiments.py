9148886c2763006e114ce3fa26aff49d7f6767dd
Tom Frederik <tlieberum@outlook.de>
2022-07-19 11:34:18 +0200

    update exp files for more parallelizability

--- crossing/run_perc_dropout_experiments.py
+++ crossing/run_perc_dropout_experiments.py
@@ -3,55 +3,46 @@ import os
 from argparse import ArgumentParser
 import time
 
-from train_dreamer_world_model import main
+from train_dreamer_world_model import main as main_dreamer
 from parsers import create_train_parser
 
-# create parser and add arguments
-train_parser: ArgumentParser = create_train_parser()
-train_parser.add_argument('--hparam_dir', type=str, default='./hparam_files/dreamer/perc_dropout/job_0')
-train_parser.add_argument('--best_hparam_file', type=str, default='./hparam_files/dreamer/best_hparams.json')
-train_parser.add_argument('--num_seeds_per_run', type=int, default=3)
-train_parser.add_argument('--reduced_volume', action='store_true', help="Reduce num settings to 1")
-train_parser.add_argument('--job_id', type=int, default=None)
-train_args = vars(train_parser.parse_args())
-
-args = {
-    "hparam_dir": train_args['hparam_dir'],
-    "best_hparam_file": train_args['best_hparam_file'],
-    "num_seeds_per_run": train_args['num_seeds_per_run'],
-    "job_id": train_args['job_id'],
-    "reduced_volume": train_args['reduced_volume'],
-}
-
-for param in args:
-    del train_args[param]
-    assert param not in train_args, f"{param} is in both args and train_args"
-
-print("hparam_dir:", args['hparam_dir'])
-print("Loading best hparams from {}".format(args['best_hparam_file']))
-
-# update with args from best hparam file
-with open(args['best_hparam_file'], 'r') as f:
-    best_hparams = json.load(f)
-train_args.update(best_hparams)
-train_args.update({"wandb_group": "dense_perc_dropout"})
-
-if args['job_id'] is None:
-    job_id = int(time.time())
-else:
-    job_id = args['job_id']
+def main(train_args):
     
-# perform runs
-for i, file in enumerate(os.listdir(args['hparam_dir'])):
-    setting_id = file.split('.')[0]
-    
+    args = {
+        "hparam_file": train_args['hparam_file'],
+        "best_hparam_file": train_args['best_hparam_file'],
+        "num_seeds_per_run": train_args['num_seeds_per_run'],
+        "job_id": train_args['job_id'],
+        "reduced_volume": train_args['reduced_volume'],
+    }
+
+    for param in args:
+        del train_args[param]
+        assert param not in train_args, f"{param} is in both args and train_args"
+
+    print("Loading best hparams from {}".format(args['best_hparam_file']))
+
+    # update with args from best hparam file
+    with open(args['best_hparam_file'], 'r') as f:
+        best_hparams = json.load(f)
+    train_args.update(best_hparams)
+    train_args.update({"wandb_group": "dense_perc_dropout"})
+
+    if args['job_id'] is None:
+        job_id = int(time.time())
+    else:
+        job_id = args['job_id']
+        
+    # perform runs
+    setting_id = args["hparam_file"].split('.')[0]
+
     if args["reduced_volume"] and setting_id not in ["7", "8", "10", "12"]:
         print(f"\nSkipping {setting_id} because of reduced_volume!")
-        continue
-    
-    with open(os.path.join(args['hparam_dir'], file), 'r') as f:
+        return
+
+    with open(os.path.join(args['hparam_dir'], args["hparam_file"]), 'r') as f:
         hparams = json.load(f)
-    
+
     for seed in range(args['num_seeds_per_run']):
         run_args = train_args.copy()
 
@@ -62,6 +53,17 @@ for i, file in enumerate(os.listdir(args['hparam_dir'])):
         
         print(f"Setting ID = {setting_id}, Running with hparams: {hparams}, seed: {seed}")
         try:
-            main(**run_args)
+            main_dreamer(**run_args)
         except Exception as e:
             print(f"Error running experiment: {e}")
+
+# create parser and add arguments
+train_parser: ArgumentParser = create_train_parser()
+train_parser.add_argument('--hparam_file', type=str, default='./hparam_files/dreamer/perc_dropout/0.json')
+train_parser.add_argument('--best_hparam_file', type=str, default='./hparam_files/dreamer/best_hparams.json')
+train_parser.add_argument('--num_seeds_per_run', type=int, default=3)
+train_parser.add_argument('--reduced_volume', action='store_true', help="Reduce num settings to 1")
+train_parser.add_argument('--job_id', type=int, default=None)
+train_args = vars(train_parser.parse_args())
+
+main(train_args)
