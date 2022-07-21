50441de02a39fd13f13bb998bb1261601f83caca
Tom Frederik <tlieberum@outlook.de>
2022-07-21 15:06:25 +0200

    writing analysis code

---
+++ analysis/fetch_preprocess_data.py
@@ -0,0 +1,48 @@
+# This file downloads the run data of the specified job ids for both settings
+# It then computes mean and std for each setting over all seeds and saves 
+# the results in a json file.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import wandb
+from tqdm import tqdm
+import json
+import os
+import sys
+sys.path.append('../')
+
+from analysis.utils import fetch_loss_list_per_setting, compute_mean_std_per_setting
+
+api = wandb.Api()
+ENT = "TomFrederik"
+NUM_SEEDS = 5
+NUM_SETTINGS = 21
+LOSS = "tuning_loss"
+
+DREAMER_PROJ = "MT-ToyTask-Dreamer"
+OURS_PROJ = "MT-ToyTask-Dreamer"
+
+DREAMER_JOB_ID = "9749161" #NOTE <-- change this for new runs
+OURS_JOB_ID = "9749161" #NOTE <-- change this for new runs
+
+dreamer_dir = f"./data/dreamer/{DREAMER_JOB_ID}"
+ours_dir = f"./data/ours/{OURS_JOB_ID}"
+
+print('Loading dreamer data...')
+if not os.path.exists(dreamer_dir):
+    os.makedirs(dreamer_dir, exist_ok=True)
+    dreamer_mean_std_per_setting = compute_mean_std_per_setting(fetch_loss_list_per_setting(api, DREAMER_JOB_ID, NUM_SETTINGS, NUM_SEEDS, DREAMER_PROJ, LOSS, ENT))
+    with open(f"{dreamer_dir}/mean_std_per_setting.json", "w") as f:
+        json.dump(dreamer_mean_std_per_setting, f)
+else:
+    print('Directory already exists -> skipping...')
+
+
+print('Loading ours data...')
+if not os.path.exists(ours_dir):
+    os.makedirs(ours_dir, exist_ok=True)
+    ours_mean_std_per_setting = compute_mean_std_per_setting(fetch_loss_list_per_setting(api, OURS_JOB_ID, NUM_SETTINGS, NUM_SEEDS, OURS_PROJ, LOSS, ENT))
+    with open(f"{ours_dir}/mean_std_per_setting.json", "w") as f:
+        json.dump(ours_mean_std_per_setting, f)
+else:
+    print('Directory already exists -> skipping...')
---
+++ analysis/utils.py
@@ -0,0 +1,60 @@
+from wandb import Api
+
+from typing import List, Dict
+from tqdm import tqdm
+import numpy as np
+
+def gen_run_id_list(
+    job_id: str, 
+    num_settings: int, 
+    num_seeds: int
+) -> List[str]:
+    my_list = []
+    for setting_num in range(num_settings):
+        for seed_num in range(num_seeds):
+            my_list.append(f"setting_{setting_num}_seed_{seed_num}_job_{job_id}")
+    return my_list
+
+def fetch_last_loss(
+    api: Api, 
+    run_id: str, 
+    project: str, 
+    loss_name: str = "tuning_loss", 
+    entity: str = 'TomFrederik'
+) -> float:
+    run = api.run(f"{entity}/{project}/{run_id}")
+    return run.history(keys=[f'Validation/{loss_name}']).iloc[-1][f'Validation/{loss_name}']
+
+
+def fetch_loss_list_per_setting(
+    api: Api,
+    job_id: str,
+    num_settings: int,
+    num_seeds: int,
+    project: str,
+    loss_name: str = "tuning_loss", 
+    entity: str = 'TomFrederik',
+    reduced_volume: bool = False
+) -> Dict:
+    out_dict = dict()
+    for setting_num in tqdm(range(num_settings)):
+        loss_list = []
+        if reduced_volume and setting_num not in [7, 10, 12]:
+            continue
+        for seed_num in range(num_seeds):
+            if setting_num == 5 and seed_num == 4:
+                print("WARNING: skipping setting 5, seed 4 -> need to run this again due to time limit")
+                continue
+            run_id = f"setting_{setting_num}_seed_{seed_num}_job_{job_id}"
+            loss = fetch_last_loss(api, run_id, project, loss_name, entity)
+            loss_list.append(loss)
+        out_dict[setting_num] = loss_list
+    return out_dict
+
+def compute_mean_std_per_setting(loss_dict: Dict) -> Dict:
+    out_dict = dict()
+    for key, val in loss_dict.items():
+        mean = np.mean(val)
+        std = np.std(val, ddof=1)
+        out_dict[key] = (mean, std)
+    return out_dict
